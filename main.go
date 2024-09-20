f1a55ade84f8de7aebf8deb1db1b99a58b6ab320
santiparra <122759810+Santiparra@users.noreply.github.com>
2024-09-20 20:12:36 -0300

    chi setup

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
 
 func main(){
-	fmt.Println("hola")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not in .env file")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+    // AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+    AllowedOrigins:   []string{"https://*", "http://*"},
+    // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"*"},
+    ExposedHeaders:   []string{"Link"},
+    AllowCredentials: false,
+    MaxAge:           300, // Maximum value not ignored by any of major browsers
+  }))
+
+	server := &http.Server{
+		Handler: router,
+		Addr: 	 ":" + portString,
+	}
+
+	fmt.Printf("Server running on port %v", portString)
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}	
 }
